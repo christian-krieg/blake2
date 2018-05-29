adce2ee78fdcaf4425ec0599943b614b4d3d4c48
Christian Krieg <krieg@ict.tuwien.ac.at>
2018-05-29 15:36:44 +0200

    Added file header to 'cleanup.sh'

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,29 @@
-#removes all generated files
+#------------------------------------------------------------------------------
+#
+# VHDL implementation of the BLAKE2 Cryptographic Hash and Message
+# Authentication Code as described by Markku-Juhani O. Saarinen and
+# Jean-Philippe Aumasson in https://doi.org/10.17487/RFC7693
+#
+# Authors:
+#
+#   Benedikt Tutzer
+#   Dinka Milovancev
+#
+#
+# Supervisors:
+#
+#   Christian Krieg
+#   Martin Mosbeck
+#   Axel Jantsch
+#
+# Institute of Computer Technology
+# TU Wien
+# April 2018
+#
+# This script removes all generated files.
+#
+#------------------------------------------------------------------------------
+#
 rm -rf work-obj* tb*.ghw hashes*
+#
+#------------------------------------------------------------------------------
